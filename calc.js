c5198fb4b3515f6126947fa40162d1d14fefe018
jegedenifemi <jegedenifemi24@gmail.com>
2022-06-21 22:50:32 +0100

    initial commit

---
+++ calc.js
@@ -0,0 +1,25 @@
+var a = Number(prompt('Input First Number'));
+var b = Number(prompt('Input Second Number'));
+var c = (prompt('Input Operator (+, -, /, *)'));
+
+if(isNaN(a)) {
+  alert('Error!, Input 1 is not a number');
+}
+if(isNaN(b)) {
+  alert('Error!, Input 2 is not a Number');
+}
+if(c !== '*' && c !== '-' && c !== '/' && c !== '+'){
+  alert('Input a valid operator (+, -, /, *)')
+}
+else if(c == '*') {
+  alert(Number(a) * Number(b));
+}
+else if (c == '-') {
+    alert(Number(a) - Number(b));
+}
+else if (c == '/') {
+  alert(Number(a) / Number(b));
+}
+else if (c == '+') {
+  alert(Number(a) + Number(b));
+}
